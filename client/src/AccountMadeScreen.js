36b40d773a3316c0316a75ecfeaff36efa920c2c
Daniel Mustard <daniel.mustard13@gmail.com>
2023-02-07 21:18:10 +0000

    guest complete component now renders once submit is clicked, data from api is passed to account made component. Need to fix data being duplicated when passed to other comp

--- client/src/AccountMadeScreen.js
+++ client/src/AccountMadeScreen.js
@@ -1,53 +1,59 @@
-import React, { Component, useEffect, useState } from 'react'
+import React from 'react'
 import Form from 'react-bootstrap/Form';
-
 //function to send form data to backend server
-class AccountMadeScreen extends Component {
-    
-    render() {
-        
-        //can put other js logic here that wont be rendered but will be interacted with
 
+export default function AccountMadeScreen(data){
+  console.log(data)
       return (
-        <Form className="MainForm" onSubmit={this.submitHandler} autoComplete='off' autoCapitalize='off' autoCorrect='off'>
-        <div className="FormElements">
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Guest Username:</Form.Label>
-            <Form.Control
-              type="text"
-              readOnly
-              name="guestFirstName"
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Guest Password:</Form.Label>
-            <Form.Control
-              type="text"
-              readOnly
-              name="guestFirstName"
-            />
-
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Account Active Until:</Form.Label>
-            <Form.Control
-              type="date"
-              readOnly
-              name="guestFirstName"
-            />
-          </Form.Group>
-
-       </div>
-     </Form>
-        
-        
-        
-            
-        
+          <Form className="MainForm" autoComplete='off' autoCapitalize='off' autoCorrect='off'>
+        <h1>Guest Account Created</h1>
+            <div className="FormElements">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Guest First Name:</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Guest First Name"
+                />
+              </Form.Group>
+  
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Guest Surname:</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Guest Name"              
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Purpose of Access:</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Purpose of access"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Guest Location:</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Guest Location"
+                />
+                <Form.Text className="text-muted">
+                  For reference, your guest is not limited to using their account in this location.
+                </Form.Text>
+              </Form.Group>
+              {/* Amount of time that guest needs access for, will need to be converted into unix time*/}
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="timeSelect">Account Duration</Form.Label>
+                <Form.Select
+                  id="disabledSelect"
+                  name="guestTimeActive">
+                  <option key = 'blankChoice' hidden value> --Select Duration-- </option>
+                  <option>1 Day</option>
+                  <option>2 Days</option>
+                  <option>5 Days</option>
+                  <optgroup label="For custom time requirements please contact IT Services"></optgroup>
+                </Form.Select>
+             </Form.Group>
+           </div>
+         </Form>    
       )
-     }
    }
-  
-  
-  export default AccountMadeScreen;
--- client/src/GuestFormNew.js
+++ client/src/GuestFormNew.js
@@ -5,8 +5,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './guestForm.css'
-import Collapse from 'react-bootstrap/Collapse';
-
+import AccountMadeScreen from './AccountMadeScreen';
 
 export default function GuestForm(){
   //state for storing our form data
@@ -20,8 +19,8 @@ export default function GuestForm(){
 
   //state for storing any errors that we want to present to frontend
   const [error, setError] = useState("");
-  const [open, setOpen] = useState(false);
-
+  const [dataCollected, setDataCollected] =useState(false);
+  const [apiReturn, setAPIReturn] =useState("");
   //stores our form data inside the USE STATE
   const handleChange = (event) =>{
     setFormData({...formData, [event.target.name]: event.target.value})
@@ -29,21 +28,22 @@ export default function GuestForm(){
 
   const handleSubmit = (event) =>{
     event.preventDefault();
+    
     //validate incoming data, check if any values are empty
     if (validateInput(formData)){
       setError(validateInput(formData))
-      setOpen(!open);
     }else{
       //here we run our axios code to submit our data to API
       postData(formData)
+      setDataCollected(true);
     }
   }
 
   //after our form data input has been validated we then post the data to our endpoint
   const postData = async(data) =>{
-    axios.post('http://192.168.1.202:5000/formData', formData)
+    axios.post('http://192.168.1.202:5000/formData', data)
       .then(response =>{
-        console.log(response.data)
+        setAPIReturn(response.data)
       })
   } 
 
@@ -54,85 +54,92 @@ export default function GuestForm(){
       }
   }}
 
+    if (dataCollected === false){
+      return(
+        <Form className="MainForm" autoComplete='off' autoCapitalize='off' autoCorrect='off' onSubmit={handleSubmit}>
+          <h1>Self Service Guest Portal</h1>
+              <Alert key="warning" variant="warning">
+                By completing this form you and your guest both agree to the IT Acceptable use policy (Link Here)
+              </Alert>
+              
+              <div className="FormElements">
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Guest First Name:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Guest First Name"
+                    name="guestFirstName"
+                    value={formData.guestFirstName}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+    
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Guest Surname:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Guest Name"
+                    name="guestSurname"
+                    value={formData.guestSurname}
+                    onChange={handleChange}                
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Purpose of Access:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Purpose of access"
+                    name="guestPurpose"
+                    value={formData.guestPurpose}
+                    onChange={handleChange} 
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Guest Location:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Guest Location"
+                    name="guestLocation"
+                    value={formData.guestLocation}
+                    onChange={handleChange} 
+                  />
+                  <Form.Text className="text-muted">
+                    For reference, your guest is not limited to using their account in this location.
+                  </Form.Text>
+                </Form.Group>
+                {/* Amount of time that guest needs access for, will need to be converted into unix time*/}
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="timeSelect">Account Duration</Form.Label>
+                  <Form.Select
+                    id="disabledSelect"
+                    name="guestTimeActive"
+                    value={formData.guestTimeActive}
+                    onChange={handleChange}
+                    placeholder='Duration'>
+                    <option key = 'blankChoice' hidden value> --Select Duration-- </option>
+                    <option>1 Day</option>
+                    <option>2 Days</option>
+                    <option>5 Days</option>
+                    <optgroup label="For custom time requirements please contact IT Services"></optgroup>
+                  </Form.Select>
+               </Form.Group>
+             <Button variant="primary" type="submit">
+               Submit
+             </Button>
+             <hr></hr>
+             {error !== "" &&
+              <Alert key="danger" variant='danger'>{error}</Alert>  
+              }
+             </div>
+           </Form>
+      )
+    } else{
+      return(
+          <AccountMadeScreen data={apiReturn}/>
+      )
+    }//else render something else
+  
 
-  return(
-    <Form className="MainForm" autoComplete='off' autoCapitalize='off' autoCorrect='off' onSubmit={handleSubmit}>
-      <h1>Self Service Guest Portal</h1>
-          <Alert key="warning" variant="warning">
-            By completing this form you and your guest both agree to the IT Acceptable use policy (Link Here)
-          </Alert>
-          
-          <div className="FormElements">
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Guest First Name:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Guest First Name"
-                name="guestFirstName"
-                value={formData.guestFirstName}
-                onChange={handleChange}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Guest Surname:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Guest Name"
-                name="guestSurname"
-                value={formData.guestSurname}
-                onChange={handleChange}                
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Purpose of Access:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Purpose of access"
-                name="guestPurpose"
-                value={formData.guestPurpose}
-                onChange={handleChange} 
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Guest Location:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Guest Location"
-                name="guestLocation"
-                value={formData.guestLocation}
-                onChange={handleChange} 
-              />
-              <Form.Text className="text-muted">
-                For reference, your guest is not limited to using their account in this location.
-              </Form.Text>
-            </Form.Group>
-            {/* Amount of time that guest needs access for, will need to be converted into unix time*/}
-            <Form.Group className="mb-3">
-              <Form.Label htmlFor="timeSelect">Account Duration</Form.Label>
-              <Form.Select
-                id="disabledSelect"
-                name="guestTimeActive"
-                value={formData.guestTimeActive}
-                onChange={handleChange}
-                placeholder='Duration'>
-                <option key = 'blankChoice' hidden value> --Select Duration-- </option>
-                <option>1 Day</option>
-                <option>2 Days</option>
-                <option>5 Days</option>
-                <optgroup label="For custom time requirements please contact IT Services"></optgroup>
-              </Form.Select>
-           </Form.Group>
-         <Button variant="primary" type="submit">
-           Submit
-         </Button>
-         <hr></hr>
-         {error !== "" &&
-          <Alert key="danger" variant='danger'>{error}</Alert>  
-          }
-         </div>
-       </Form>
-  )
 
 }
 
