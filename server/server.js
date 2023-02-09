f4792e973aea19066b44e4653396fa73bb53007d
Daniel Mustard <daniel.mustard13@gmail.com>
2023-02-09 14:29:37 +0000

    server changes + we now generate passwords in code

--- server/server.js
+++ server/server.js
@@ -2,22 +2,20 @@ const express = require('express')
 const cors = require("cors");
 const ldap = require('ldapjs')
 const fs = require('fs')
-const https = require('https')
 
 
 const app = express()
 
 //allows us to parse incoming json data from body
 app.use(express.json());
-
 app.use(cors({ origin: "http://localhost:3000" }));
 
 app.listen(5000, ()=> {console.log("Server Started on port 5000")})
 
 app.post('/formData',async (req, res) =>{
-  await makeUser(req.body).then (data =>{
-    res.send(data)
-  })
+   await makeUser(req.body).then (data =>{
+     res.send(data)
+   })
 })
 
 //User should only be able to perform any of the steps below after they have authenticated with AAD, !SET THIS UP LATER
@@ -48,34 +46,52 @@ const adDateCalcFunction = (timeValue) =>{
 
 
 async function makeUser(guest){
+    //console.log(guest)
     let errorLog;
     let adDisplayName = `(Guest) ${guest.guestFirstName} ${guest.guestSurname}`
 
     //including a random integer at end of guest account to avoid possible clashes
     let username = `gst_${guest.guestSurname}${Math.floor(Math.random() * 1001)}`
 
-    let password = "rayate7ubU-r"
-    //need to use unicode password to send to AD:
-    unicodePwd = Buffer.from('"'+password+'"',"utf16le").toString();
-    
-    //data to pass to AD: , we cannot 
-    var entry = {
-        displayName: adDisplayName,   
-        sAMAccountName: username,   
-        name: guest.guestFirstName,
-        unicodePwd: unicodePwd,
-        userAccountControl:'512', //this makes the account active
-        accountExpires: adDateCalcFunction(guest.accountDuration),
-        sn: guest.guestSurname,
-        givenName: guest.guestFirstName,
-        userPrincipalName: `${username}@dandomain.com`,    
-        company: "dandomain.com",   
-        info: `GUEST ACCOUNT, Purpose: ${guest.purposeOfAccess}`,
-        department: "GUEST",   
-        objectClass: ['organizationalPerson', 'person', 'top', 'user'],
-        //objectCategory: personCategory,
-        description: 'Guest account created: ' + (new Date()).toLocaleString()
+    function generatePassword() {
+      // Character set to be used for password generation
+      const charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      // Variable to store the generated password
+      let randomPassword = '';
+      // Loop to generate the password 8 characters long
+      for (let i = 0; i < 11; i++) {
+        // Generate a random index for the character set
+        let randomPoz = Math.floor(Math.random() * charSet.length);
+        // Add the character at the randomly generated index to the password
+        randomPassword += charSet.substring(randomPoz, randomPoz + 1);
       }
+      // Return the generated password
+      return randomPassword;
+    }
+    
+    randomPassword = generatePassword();
+    
+    //console.log(password())
+    //need to use unicode password to send to AD:
+    unicodePwd = Buffer.from('"'+randomPassword+'"',"utf16le").toString();
+    
+    //data to pass to AD: , we cannot
+     var entry = {
+         displayName: adDisplayName,   
+         sAMAccountName: username,   
+         name: guest.guestFirstName,
+         unicodePwd: unicodePwd,
+         userAccountControl:'512', //this makes the account active
+         accountExpires: adDateCalcFunction(guest.accountDuration),
+         sn: guest.guestSurname,
+         givenName: guest.guestFirstName,
+         userPrincipalName: `${username}@dandomain.com`,    
+         company: "dandomain.com",   
+         info: `GUEST ACCOUNT, Purpose: ${guest.purposeOfAccess}`,
+         department: "GUEST",   
+         objectClass: ['organizationalPerson', 'person', 'top', 'user'],
+         description: 'Guest account created: ' + (new Date()).toLocaleString()
+       }
       //new user DN decides ou that user will go into, this case we have OU called GUEST
       const newDN = `cn=${adDisplayName},OU=Guests,DC=dandomain,DC=com`
       
@@ -88,6 +104,7 @@ async function makeUser(guest){
       const client = ldap.createClient({
         url: 'ldaps://dandc1.dandomain.com',
         tlsOptions: tlsOptions,
+        reconnect: true //if connection is lost with ldap server we auto reconnect
       });
       //passing credentials
       client.bind('CN=guestAdm,OU=Guests,DC=dandomain,DC=com', 'Pscxkufx1', (err) => {
@@ -99,26 +116,32 @@ async function makeUser(guest){
         }
       });
 
-      client.add ( newDN, entry, (err, result)=>{
-        // console.log(result)
-        if (err){
-          console.log(err)
-          err + errorLog;
-        }else{
-        }
-      });
-
-      //closing connection with ldap server, prevents timeout crash from server
-      client.unbind();
+       client.add ( newDN, entry, (error)=>{
+         // console.log(result)
+         if (error){
+           console.log(error.toString())
+           //err + errorLog;
+         }else{
+           console.log('Generated account with no errors')
+           //closing connection with ldap server, prevents timeout crash from server
+           client.unbind()
+           client.destroy();
+         }
+       });
+      //handling any ldap client connection error (stops server from crashing if AD terminated LDAP connection)
+      client.on('error', (err) => {
+        console.log(err.message) // this will be your ECONNRESET message
+      })
   if (errorLog == null){
     return {
-      status: 'Account Made successfully',
+      status: 201,
       username: `${username}`,
-      password: `${password}`
+      password: `${randomPassword}`,
     }
+    
   }else{
     return {
-      status: 'Error Processing request'
+      status: 409
     }
   }
 }
