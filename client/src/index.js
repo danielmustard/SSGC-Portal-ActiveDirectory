a5cd3ab416fc0d2f612f529df722106c9f10f342
Dan <61760657+danielmustard@users.noreply.github.com>
2023-02-13 08:53:43 +0000

    -last commit before adding Azure Auth-

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,6 @@ import GuestFormNew from './GuestFormNew'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App/> */}
-    {/* <AccountMadeScreen /> */}
-    {/* <GuestForm /> */}
     <GuestFormNew/>
   </React.StrictMode>
 );
